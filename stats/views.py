15f4865dc38077dea0ee63e7b3be0c4988f8f737
Андрей Журавлев <info@ssh66.ru>
2025-08-15 00:31:26 +0500

    add _update_stats_active_mail

--- stats/views.py
+++ stats/views.py
@@ -1,22 +1,23 @@
 import os
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ssh_control_mail_server.settings import PYTHON_PATH, BASE_DIR, DEBUG
 from white_list_ip.models import WhiteListIP
-from .models import StatsLoginMail, StatsServerMail, StatsBaseMail
+from .models import StatsLoginMail, StatsServerMail, StatsBaseMail, StatsActiveMail, StatsIPMail
 
 INFO_EMAIL = 'info@himmetproduct.ru'
 
 class UpdateStatsView(APIView):
     def get(self, request):
         current_date = date.today().isoformat()
-
         result = self._update_stats_login()
         self._update_stats_server_mail()
         self._update_stats_base_mail(current_date)
+        self._update_stats_active_mail(current_date)
+        # self._update_stats_ip_mail(current_date)
         return Response({'result': result})
     
     def _update_stats_login(self):
@@ -126,3 +127,48 @@ class UpdateStatsView(APIView):
                 new_base_stat.count_output_email=count_output_email
                 new_base_stat.count_input_info_email=count_input_info_email
                 new_base_stat.save()
+
+    def _update_stats_active_mail(self, current_date):
+        server_email_list = StatsServerMail.objects.order_by('email').distinct().values_list('email')
+        for item in server_email_list[:1]:
+            item = item[0]
+            item_name = StatsServerMail.objects.get(email=item).name
+            if not DEBUG:
+                cmd = f'cd {BASE_DIR}/;sudo cat /var/log/dovecot/imap.log | grep "> <{item}>" | grep "imap-login: Login" | grep {current_date} > _tmp_result.txt'
+                os.system(cmd)
+
+            file = open(f"{BASE_DIR}/_tmp_result.txt", "r")
+            content_lines = file.read().split('\n')
+            file.close()
+            date_start_active = ""
+            for line in content_lines:
+                if line:
+                    line_items = line.split(' ')
+                    if not date_start_active:
+                        date_start_active = datetime.fromisoformat(line_items[0])
+                        date_end_active = date_start_active
+
+                        new_active_stat = StatsActiveMail.objects.filter(email=item, name=item_name, date_start_active=date_start_active).first()
+                        if not new_active_stat:
+                            new_active_stat = StatsActiveMail(email=item, name=item_name)
+                        new_active_stat.date_start_active = date_start_active
+                        new_active_stat.date_end_active = date_end_active
+                        new_active_stat.save()
+                    else:
+                        tmp_date = datetime.fromisoformat(line_items[0])
+                        if abs(tmp_date - date_end_active) < timedelta(minutes=5):
+                            date_end_active = tmp_date
+                        else:
+                            new_active_stat.date_end_active = date_end_active
+                            new_active_stat.save()
+                            date_start_active = tmp_date
+                            date_end_active = date_start_active
+
+                            new_active_stat = StatsActiveMail.objects.filter(email=item, name=item_name, date_start_active=date_start_active).first()
+                            if not new_active_stat:
+                                new_active_stat = StatsActiveMail(email=item, name=item_name)
+                            new_active_stat.date_start_active = date_start_active
+                            new_active_stat.date_end_active = date_start_active
+                            new_active_stat.save()
+            new_active_stat.date_end_active = date_end_active
+            new_active_stat.save()
